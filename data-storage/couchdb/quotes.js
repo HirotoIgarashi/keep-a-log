6f506c13b4001bf08063d3d3eb262a6237f66e8d
HirotoIgarashi <HirotoIgarashi@gmail.com>
2021-09-05 14:26:42 +0900

    update cradle

---
+++ data-storage/couchdb/quotes.js
@@ -0,0 +1,27 @@
+var cradle = require('cradle');
+
+var db = new(cradle.Connection)().database('quotes');
+var params = { author: process.argv[2], quote: process.argv[3] };
+
+function errorHandler(err) {
+  if (err) {
+    console.log(err);
+    process.exit();
+  }
+}
+
+function checkAndSave(err) {
+  errorHandler(err);
+  if (params.author && params.quote ) {
+    db.save({ author: params.author, quote: params.quote }, errorHandler);
+  }
+}
+
+// データベースに書き込む前に、データベースが存在するか確認する
+db.exists(function(err, exists) {
+  errorHandler(err);
+  if (!exists) {
+    db.create(checkAndSave);
+    return;
+  }
+});

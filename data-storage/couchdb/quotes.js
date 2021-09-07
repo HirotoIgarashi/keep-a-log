a4ec7f6c4575f4e9147714ab465eac77dadbc055
HirotoIgarashi <HirotoIgarashi@gmail.com>
2021-09-08 02:28:12 +0900

    update couchdb

--- data-storage/couchdb/quotes.js
+++ data-storage/couchdb/quotes.js
@@ -6,7 +6,15 @@ var db = new(cradle.Connection)(
   { auth: { username: 'admin', password: 'password' } }
 ).database('quotes');
 
-var params = { author: process.argv[2], quote: process.argv[3] };
+//var db = new (cradle.Connection)({
+// secure: true,
+//  auth: { username: 'admin', password: 'password' }
+// }).database('quotes');
+
+var params = {
+  author: process.argv[2],
+  quote: process.argv[3]
+};
 
 function errorHandler(err) {
   if (err) {
@@ -15,10 +23,40 @@ function errorHandler(err) {
   }
 }
 
+function createQuotesView(err) {
+  errorHandler(err);
+  db.save('_design/quotes', {
+    views: { byAuthor: {
+       map: 'function (doc) { emit(doc.author, doc)}'}}
+  }, outputQuotes);
+}
+
+function outputQuotes (err) {
+  errorHandler(err);
+  if (params.author) {
+    db.view('quotes/byAuthor', { key: params.author },
+    function (err, rowsArray) {
+      if (err && err.error === 'not_found') {
+        createQuotesView();
+        return;
+      }
+      errorHandler(err);
+      rowsArray.forEach(function (doc) {
+        console.log('%s: %s', doc.author, doc.quote);
+        return;
+      });
+    })
+  }
+}
+
 function checkAndSave(err) {
   errorHandler(err);
   if (params.author && params.quote ) {
-    db.save({ author: params.author, quote: params.quote }, errorHandler);
+    db.save(
+      { author: params.author, quote: params.quote },
+      outputQuotes
+  );
+    outputQuotes;
   }
 }
 

79724a83510137df90ec49317f38f0fed4dbcaf8
HirotoIgarashi <HirotoIgarashi@gmail.com>
2021-09-16 12:53:28 +0900

    update file-system

--- file-system/index.js
+++ file-system/index.js
@@ -1,12 +1,13 @@
 'use strice';
 
 const { extname } = require('path');
-const { readdir, readFile, writeFile } = require('fs').promises;
+const { readdir, readFile, writeFile, unlink } = require('fs').promises;
 
 exports.fetchAll = async () => {
   // 同一ディレクスト内に存在するJSONファイルをすべて取得
   const files = (await readdir(__dirname))
     .filter(file => extname(file) === '.json')
+
   return Promise.all(
     files.map(file =>
       readFile(`${__dirname}/${file}`, 'utf8').then(JSON.parse)
@@ -16,4 +17,23 @@ exports.fetchAll = async () => {
 
 exports.create = user =>
   writeFile(`${__dirname}/${user.id}.json`, JSON.stringify(user))
-  
+
+exports.update = async (id, update) => {
+  const fileName = `${__dirname}/${id}.json`
+  return readFile(fileName, 'utf8').then(
+    content => {
+      const user = {
+        ...JSON.parse(content),
+        ...update
+      }
+    },
+    err => err.code === 'ENOENT' ? null : Promise.reject(err)
+  )
+}
+
+exports.remove = id => unlink(`${__dirname}/${id}.json`)
+  .then(
+    () => id,
+    // ファイルが存在しない場合はnullを返し、それ以外はそのままエラーにする
+    err => err.code === 'ENOENT' ? null : Promise.reject(err)
+  )

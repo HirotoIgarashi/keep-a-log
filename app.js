237823453876bfbd3e2d41a5a3d738b5f8527a29
Hiroto Igarashi <hirotoigarashi@gmail.com>
2025-05-01 02:24:00 +0900

    update app.js

--- app.js
+++ app.js
@@ -5,18 +5,18 @@
 
 // -----------------------------------------------------------------------------
 // ---------------- モジュールスコープ変数開始 ---------------------------------
-const express        = require('express');   // expressのモジュールをロードする
-const app            = express();     // expressアプリケーションをapp定数に代入
-const logger         = require('morgan');
+const express = require('express');   // expressのモジュールをロードする
+const app = express();     // expressアプリケーションをapp定数に代入
+const logger = require('morgan');
 const methodOverride = require('method-override');
 
-const port           = 8000    // 待ち受けるポートを8000に設定する
-const path           = require('path');
-const favicon        = require('serve-favicon');
-const connectFlash   = require('connect-flash');
+const port = 8000    // 待ち受けるポートを8000に設定する
+const path = require('path');
+const favicon = require('serve-favicon');
+const connectFlash = require('connect-flash');
 // セッションのタイムアウト時間を30日に設定する
 //                     1秒  * 分 * 時 * 日 * 30日
-const expire_time    = 1000 * 60 * 60 * 24 * 30;
+const expire_time = 1000 * 60 * 60 * 24 * 30;
 // ---------------- モジュールスコープ変数終了 ---------------------------------
 
 // ---------------- サーバ構成開始 ---------------------------------------------
@@ -35,27 +35,27 @@ app.use(express.static('public'));    // appの設定 start
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 // sessionの処理
-const session        = require('express-session');    // sessionの設定
-const RedisStore     = require('connect-redis')(session);
+const session = require('express-session');    // sessionの設定
+const RedisStore = require('connect-redis')(session);
 // TODO: redisのバージョンは3.0.0である必要があります
-const redisClient    = require('redis').createClient();
+const redisClient = require('redis').createClient();
 app.set('trust proxy', 1); // trust first proxy
 app.use(session(
-  { 
-    store             : new RedisStore({ client: redisClient}),
-    secret            : 'keepalog',
-    resave            : false,
-    saveUninitialized : false,
-    cookie : {
-      secure   : false,
-      httpOnly : false,
-      expires  : new Date(Date.now() + expire_time)
-    } 
+  {
+    store: new RedisStore({ client: redisClient }),
+    secret: 'keepalog',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false,
+      httpOnly: false,
+      expires: new Date(Date.now() + expire_time)
+    }
   }
 ));
 // URLエンコードされたデータを解析する
 app.use(express.json());
-app.use(express.urlencoded( { extended: false } ));
+app.use(express.urlencoded({ extended: false }));
 app.use(connectFlash());      // connect-flashをミドルウェアとして使う
 // フラッシュメッセージをレスポンスのローカル変数flashMessagesに代入
 app.use((req, res, next) => {
@@ -75,8 +75,8 @@ let server;
 if (process.env.NODE_ENV === 'test') {
   server = app.listen('8001', () => {
     console.log(
-'Server Message: The Express.js server has started and is listening on port \
-number:' + `${app.get('port')}`); 
+      'Server Message: The Express.js server has started and is listening on port \
+number:' + `${app.get('port')}`);
   });
 }
 else {
@@ -84,9 +84,9 @@ else {
     port,
     () => {
       console.log(
-'Server Message: The Express.js server has started and is listening on port \
+        'Server Message: The Express.js server has started and is listening on port \
 number:' + port
-      ); 
+      );
     }
   );
 }
